422735a9c8cd5b86776b717eebeab41bc6827bd1
PradeepManimaran <pradeepanbu1994@gmail.com>
2021-11-11 22:57:44 +0530

    pdp

--- src/org/company/CompanyInfo.java
+++ src/org/company/CompanyInfo.java
@@ -10,11 +10,15 @@ public class CompanyInfo {
 	private void companyadr() {
 		System.out.println("Company Address");
 	}
+	private void companyhr() {
+		System.out.println("Company hr address");
+	}
 	public static void main(String[] args) {
 		CompanyInfo c = new CompanyInfo();
 		c.companyName();
 		c.companyid();
 		c.companyadr();
+		c.companyhr();
 		
 	}
 
